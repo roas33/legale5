246560497f6340dcffc196bc8a156cdf9b30604c
roas33 <salvatore.roas@gmail.com>
2025-02-23 20:27:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,101 @@
+{\rtf1\ansi\deff3\adeflang1025
+{\fonttbl{\f0\froman\fprq2\fcharset0 Times New Roman;}{\f1\froman\fprq2\fcharset2 Symbol;}{\f2\fswiss\fprq2\fcharset0 Arial;}{\f3\froman\fprq2\fcharset0 Liberation Serif{\*\falt Times New Roman};}{\f4\fnil\fprq0\fcharset0 inter;}{\f5\fswiss\fprq2\fcharset0 Liberation Sans{\*\falt Arial};}{\f6\fnil\fprq2\fcharset0 Microsoft YaHei;}{\f7\fnil\fprq2\fcharset0 Lucida Sans;}{\f8\fswiss\fprq0\fcharset0 Lucida Sans;}}
+{\colortbl;\red0\green0\blue0;\red0\green0\blue255;\red0\green255\blue255;\red0\green255\blue0;\red255\green0\blue255;\red255\green0\blue0;\red255\green255\blue0;\red255\green255\blue255;\red0\green0\blue128;\red0\green128\blue128;\red0\green128\blue0;\red128\green0\blue128;\red128\green0\blue0;\red128\green128\blue0;\red128\green128\blue128;\red192\green192\blue192;\red50\green50\blue53;}
+{\stylesheet{\s0\snext0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052 Normal;}
+{\*\cs15\snext15 Caratteri nota a pi\u232\'e8 di pagina;}
+{\*\cs16\snext16 Caratteri nota di chiusura;}
+{\*\cs17\snext17\rtlch\alang255 \ltrch\lang255\langfe255\loch\cf9\lang255\ul\ulc0\dbch\langfe255 Collegamento Internet;}
+{\*\cs18\snext18\rtlch\alang255 \ltrch\lang255\langfe255\loch\cf13\lang255\ul\ulc0\dbch\langfe255 Collegamento Internet visitato;}
+{\s19\sbasedon0\snext20\rtlch\af7\afs28 \ltrch\hich\af5\loch\sb240\sa120\keepn\f5\fs28\dbch\af6 Titolo;}
+{\s20\sbasedon0\snext20\loch\sl276\slmult1\sb0\sa140 Corpo del testo;}
+{\s21\sbasedon20\snext21\rtlch\af8 \ltrch\loch\sl276\slmult1\sb0\sa140 Elenco;}
+{\s22\sbasedon0\snext22\rtlch\af8\afs24\ai \ltrch\loch\sb120\sa120\noline\fs24\i Didascalia;}
+{\s23\sbasedon0\snext23\rtlch\af8\alang255 \ltrch\lang255\langfe255\loch\noline\lang255\dbch\langfe255 Indice;}
+{\s24\sbasedon0\snext24\loch\li567\ri0\lin567\rin0\fi0 Contenuto elenco;}
+}{\*\generator LibreOffice/7.2.2.2$Windows_X86_64 LibreOffice_project/02b2acce88a210515b4a5bb2e46cbfb63fe97d56}{\info{\creatim\yr2025\mo2\dy23\hr9\min24}{\revtim\yr2025\mo2\dy23\hr20\min6}{\printim\yr0\mo0\dy0\hr0\min0}}{\*\userprops}\deftab709
+\hyphauto1\viewscale120
+{\*\pgdsctbl
+{\pgdsc0\pgdscuse451\pgwsxn11906\pghsxn16838\marglsxn1134\margrsxn1134\margtsxn1134\margbsxn1134\pgdscnxt0 Stile di pagina predefinito;}}
+\formshade\paperh16838\paperw11906\margl1134\margr1134\margt1134\margb1134\sectd\sbknone\pgndec\sftnnar\saftnnrlc\sectunlocked1\pgwsxn11906\pghsxn16838\marglsxn1134\margrsxn1134\margtsxn1134\margbsxn1134\ftnbj\ftnstart1\ftnrstcont\ftnnar\aenddoc\aftnrstcont\aftnstart1\aftnnrlc
+{\*\ftnsep\chftnsep}\pgndec\pard\plain \s0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052\loch\ql\widctlpar\li0\ri0\lin0\rin0\fi0\ltrpar{
+ }
+\par \pard\plain \s0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052\loch\ql\widctlpar\li0\ri0\lin0\rin0\fi0\ltrpar\loch
+
+\par \pard\plain \s0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052\loch\ql\widctlpar\li0\ri0\lin0\rin0\fi0\ltrpar{\loch
+async function textToSpeech(text) \{}
+\par \pard\plain \s0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052\loch\ql\widctlpar\li0\ri0\lin0\rin0\fi0\ltrpar{
+    }{\loch
+const apiKey = "}{\hich\af4\loch\scaps0\caps0\cf17\fs19\expnd0\expndtw0\i0\b0\f4\loch
+sk_231812647c210b28e918c5093cc3232184db294928226d28}{\loch
+  ";}
+\par \pard\plain \s0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052\loch\ql\widctlpar\li0\ri0\lin0\rin0\fi0\ltrpar{
+    }{\loch
+const voiceID = "ID_DELLA_VOCE_SCELTA"; }
+\par \pard\plain \s0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052\loch\ql\widctlpar\li0\ri0\lin0\rin0\fi0\ltrpar\loch
+
+\par \pard\plain \s0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052\loch\ql\widctlpar\li0\ri0\lin0\rin0\fi0\ltrpar{
+    }{\loch
+const response = await fetch("https://api.elevenlabs.io/v1/text-to-speech/" + voiceID, \{}
+\par \pard\plain \s0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052\loch\ql\widctlpar\li0\ri0\lin0\rin0\fi0\ltrpar{
+        }{\loch
+method: "POST",}
+\par \pard\plain \s0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052\loch\ql\widctlpar\li0\ri0\lin0\rin0\fi0\ltrpar{
+        }{\loch
+headers: \{}
+\par \pard\plain \s0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052\loch\ql\widctlpar\li0\ri0\lin0\rin0\fi0\ltrpar{
+            }{\loch
+"Content-Type": "application/json",}
+\par \pard\plain \s0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052\loch\ql\widctlpar\li0\ri0\lin0\rin0\fi0\ltrpar{
+            }{\loch
+"xi-api-key": apiKey}
+\par \pard\plain \s0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052\loch\ql\widctlpar\li0\ri0\lin0\rin0\fi0\ltrpar{
+        }{\loch
+\},}
+\par \pard\plain \s0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052\loch\ql\widctlpar\li0\ri0\lin0\rin0\fi0\ltrpar{
+        }{\loch
+body: JSON.stringify(\{}
+\par \pard\plain \s0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052\loch\ql\widctlpar\li0\ri0\lin0\rin0\fi0\ltrpar{
+            }{\loch
+text: text,}
+\par \pard\plain \s0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052\loch\ql\widctlpar\li0\ri0\lin0\rin0\fi0\ltrpar{
+            }{\loch
+voice_settings: \{ stability: 0.5, similarity_boost: 0.5 \}}
+\par \pard\plain \s0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052\loch\ql\widctlpar\li0\ri0\lin0\rin0\fi0\ltrpar{
+        }{\loch
+\})}
+\par \pard\plain \s0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052\loch\ql\widctlpar\li0\ri0\lin0\rin0\fi0\ltrpar{
+    }{\loch
+\});}
+\par \pard\plain \s0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052\loch\ql\widctlpar\li0\ri0\lin0\rin0\fi0\ltrpar\loch
+
+\par \pard\plain \s0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052\loch\ql\widctlpar\li0\ri0\lin0\rin0\fi0\ltrpar{
+    }{\loch
+const audioBlob = await response.blob();}
+\par \pard\plain \s0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052\loch\ql\widctlpar\li0\ri0\lin0\rin0\fi0\ltrpar{
+    }{\loch
+const audioUrl = URL.createObjectURL(audioBlob);}
+\par \pard\plain \s0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052\loch\ql\widctlpar\li0\ri0\lin0\rin0\fi0\ltrpar{
+    }
+\par \pard\plain \s0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052\loch\ql\widctlpar\li0\ri0\lin0\rin0\fi0\ltrpar{
+    }{\loch
+const audio = new Audio(audioUrl);}
+\par \pard\plain \s0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052\loch\ql\widctlpar\li0\ri0\lin0\rin0\fi0\ltrpar{
+    }{\loch
+audio.play();}
+\par \pard\plain \s0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052\loch\ql\widctlpar\li0\ri0\lin0\rin0\fi0\ltrpar{\loch
+\}}
+\par \pard\plain \s0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052\loch\ql\widctlpar\li0\ri0\lin0\rin0\fi0\ltrpar\loch
+
+\par \pard\plain \s0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052\loch\ql\widctlpar\li0\ri0\lin0\rin0\fi0\ltrpar{\loch
+// Esempio di utilizzo:}
+\par \pard\plain \s0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052\loch\ql\widctlpar\li0\ri0\lin0\rin0\fi0\ltrpar{\loch
+document.getElementById("speakButton").addEventListener("click", function() \{}
+\par \pard\plain \s0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052\loch\ql\widctlpar\li0\ri0\lin0\rin0\fi0\ltrpar{
+    }{\loch
+const userText = document.getElementById("textInput").value;}
+\par \pard\plain \s0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052\loch\ql\widctlpar\li0\ri0\lin0\rin0\fi0\ltrpar{
+    }{\loch
+textToSpeech(userText);}
+\par \pard\plain \s0\rtlch\af7\afs24\alang1081 \ltrch\lang1040\langfe2052\hich\af3\loch\widctlpar\hyphpar0\aspalpha\ltrpar\cf0\f3\fs24\lang1040\kerning1\dbch\af9\langfe2052\loch\ql\widctlpar\li0\ri0\lin0\rin0\fi0\ltrpar{\loch
+\});}
+\par }
